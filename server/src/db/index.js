f82ecf29f6312555b3b473c6643c7915322a1cdd
Vamsee Krishna Padala <vamseepadala4@gmail.com>
2026-06-30 22:48:50 +0530

    Remove hardcoded dotenv ,add SSL config for Railway Postgres

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,13 +1,12 @@
-require('dotenv').config({ path: require('path').join(__dirname, '../../.env') });
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 pool.on('connect', () => {
   console.log('Connected to PostgreSQL');
-  console.log('DB URL:', process.env.DATABASE_URL); // temporary debug line
 });
 
 pool.on('error', (err) => {
